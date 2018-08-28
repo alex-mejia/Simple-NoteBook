2ea44792e9b0b03bc9e7a4fbdd4f894934bc1dca
Alex Mejia <mejialex@gmail.com>
2018-08-28 13:13:41 -0600

    estilizado de frames y controles de items agregados

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.font import Font
 
 class Gui:
     def __init__(self,raiz):
@@ -12,27 +13,45 @@ class Gui:
         self._frame_botton = Frame(raiz)
         self._frame_botton.grid(row=1, column=0, sticky=W + E + N + S)
         self._frame_botton.rowconfigure(0, weight=1)
-        self._frame_botton.columnconfigure(0, weight=1)
-        self._frame_botton.columnconfigure(1, weight=3)
+        self._frame_botton.columnconfigure(0, weight=0)
+        self._frame_botton.columnconfigure(1, weight=2)
 
         # frame donde va el indice y las herramientas del indice
         self._frame_left = Frame(self._frame_botton)
         self._frame_left.grid(row=0, column=0, sticky=W + E + N + S)
-        self._frame_left.rowconfigure(0,weight=1)
+        self._frame_left.rowconfigure(0,weight=0)
         self._frame_left.rowconfigure(1, weight=5)
         self._frame_left.columnconfigure(0,weight=1)
 
         # frame donde van las herramientas de los items
         self._frame_left_top = Frame(self._frame_left, bg="#9999ff")
-        self._frame_left_top.grid(row=0,column=0,sticky=N + S + W + E)
+        self._frame_left_top.grid(row=0,column=0,sticky=N + S + W + E,padx=1,pady=1)
 
         # frame donde van los items del inidice
         self._frame_left_botton = Frame(self._frame_left, bg='#b3b3ff')
-        self._frame_left_botton.grid(row=1, column=0, sticky= N + S + W + E)
+        self._frame_left_botton.grid(row=1, column=0, sticky= N + S + W + E,padx=1,pady=1)
+        self._frame_left_botton.columnconfigure(0,weight=1)
 
         # frame donde va la nota
         self._frame_right = Frame(self._frame_botton, bg='#ffff99')
         self._frame_right.grid(row=0, column=1, sticky=W + E + N + S)
 
+        self.area_control_items()
+        self.area_indices()
+
     def area_control_items(self):
-       pass
+       entry_item = Entry(self._frame_left_top,width=30)
+       entry_item.grid(row=0,column=0,padx=2,pady=10)
+
+       chk_es_seccion = Checkbutton(self._frame_left_top, text="Seccion", bg="#9999ff")
+       chk_es_seccion.grid(row=0, column=1, pady=10)
+
+    def area_indices(self):
+        lst_items = Listbox(self._frame_left_botton, bg='#b3b3ff',borderwidth=0, highlightthickness=0,
+                            font=Font(family="Sans Serif", size=11))
+        lst_items.grid(row=0,column=0,sticky= W + E,padx=5,pady=5)
+
+
+
+
+
