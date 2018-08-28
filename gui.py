497e00089730a7ff03b59a97b44df870a860b838
Alex Mejia <mejialex@gmail.com>
2018-08-28 10:39:12 -0600

    ventana centrada y frames finalizados

--- gui.py
+++ gui.py
@@ -5,25 +5,34 @@ class Gui:
         self._raiz = raiz
 
         # maneja el frame top donde iran los cuadernos
-        self._frame_top = Frame(self._raiz,bg='red')
+        self._frame_top = Frame(self._raiz,bg='black')
         self._frame_top.grid(row=0, column=0, sticky=W + E + N + S)
 
         # contenedor para la nota y el indice
-        self._frame_botton = Frame(raiz, bg='blue')
+        self._frame_botton = Frame(raiz)
         self._frame_botton.grid(row=1, column=0, sticky=W + E + N + S)
         self._frame_botton.rowconfigure(0, weight=1)
         self._frame_botton.columnconfigure(0, weight=1)
         self._frame_botton.columnconfigure(1, weight=3)
 
         # frame donde va el indice y las herramientas del indice
-        self._frame_left = Frame(self._frame_botton, bg='yellow')
+        self._frame_left = Frame(self._frame_botton)
         self._frame_left.grid(row=0, column=0, sticky=W + E + N + S)
+        self._frame_left.rowconfigure(0,weight=1)
+        self._frame_left.rowconfigure(1, weight=5)
+        self._frame_left.columnconfigure(0,weight=1)
+
+        # frame donde van las herramientas de los items
+        self._frame_left_top = Frame(self._frame_left, bg="#9999ff")
+        self._frame_left_top.grid(row=0,column=0,sticky=N + S + W + E)
+
+        # frame donde van los items del inidice
+        self._frame_left_botton = Frame(self._frame_left, bg='#b3b3ff')
+        self._frame_left_botton.grid(row=1, column=0, sticky= N + S + W + E)
 
         # frame donde va la nota
-        self._frame_right = Frame(self._frame_botton, bg='green')
+        self._frame_right = Frame(self._frame_botton, bg='#ffff99')
         self._frame_right.grid(row=0, column=1, sticky=W + E + N + S)
-
-       # TODO agregar frame de las herramientas del indice
 
     def area_control_items(self):
        pass
--- main.py
+++ main.py
@@ -2,14 +2,30 @@
 from gui import *
 
 raiz = Tk()
+
+def centrar_ventana():
+    width_of_window = 1000
+    height_of_window = 600
+
+    screen_width = raiz.winfo_screenwidth()
+    screen_height = raiz.winfo_screenheight()
+
+    x_coordinate = (screen_width/2) - (width_of_window/2)
+    y_coordinate = (screen_height / 2) - (height_of_window / 2)
+
+    return x_coordinate,y_coordinate
+
+xpos = int(centrar_ventana()[0])
+ypos = int(centrar_ventana()[1])
+
+
 raiz.configure(background='black')
-raiz.geometry("1000x600")
+raiz.geometry(f"1000x600+{xpos}+{ypos}")
 raiz.columnconfigure(0, weight=1)
 raiz.rowconfigure(0, weight=1)
-raiz.rowconfigure(1, weight=10)
-
-# TODO centrar la ventana
+raiz.rowconfigure(1, weight=9)
 
 ui = Gui(raiz)
 
 raiz.mainloop()
+
